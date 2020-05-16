8be35841c1a13e17ab0d5720a4b3a70107758e68
Evan <62257610+evanlax9@users.noreply.github.com>
2020-05-15 20:40:41 -0400

    Updated Portfolio

---
+++ assets/handleform.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    //     $("#subButton").click(function () {
+
+    //         $("#contactForm").submit(function (event) {
+    //             event.preventDefault();
+    //             $(this).attr("action", "mailto:evanlax9@gmail.com?subject=ContactFormSubmitted");
+    //             console.log($(this).action);
+    //             return true;
+
+
+
+    //         })
+
+    //     })
+
+
+})
